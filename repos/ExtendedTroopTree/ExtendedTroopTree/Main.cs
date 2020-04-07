0411137f06a82be163aabfd757f8206e51ccb053
KaleJannings <kylejennings5@gmail.com>
2020-04-06 20:29:28 -0700

    Project Setup
    This is the starting point, everything before this was a mistake.

---
+++ repos/ExtendedTroopTree/ExtendedTroopTree/Main.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtendedTroopTree
+{
+    public class Class1
+    {
+    }
+}
