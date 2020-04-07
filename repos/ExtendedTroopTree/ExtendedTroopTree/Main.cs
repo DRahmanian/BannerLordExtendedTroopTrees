d1d9713f8a1ed2c313a28f8731090073148988a3
KaleJannings <kylejennings5@gmail.com>
2020-04-06 20:38:57 -0700

    Click me Menu Option

--- repos/ExtendedTroopTree/ExtendedTroopTree/Main.cs
+++ repos/ExtendedTroopTree/ExtendedTroopTree/Main.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaleWorlds.Core;
+using TaleWorlds.Localization;
+using TaleWorlds.MountAndBlade;
 
 namespace ExtendedTroopTree
 {
-    public class Class1
+    public class Main : MBSubModuleBase
     {
+        protected override void OnSubModuleLoad()
+        {
+            Module.CurrentModule.AddInitialStateOption(new InitialStateOption("TestMainMenuOption", new TextObject("Click me!", null), 9990, () =>
+            {
+                InformationManager.DisplayMessage(new InformationMessage("Hello World!"));
+            }, false));
+        }
     }
 }
